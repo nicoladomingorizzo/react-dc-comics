bc81655ed17469a5942a5850698412154ffc8be2
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-15 13:17:57 +0200

    add jumbotron and load more button; refactor AppMainLinks to use a dynamic links array

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,11 +1,30 @@
-import AppMainLinks from './AppMainLinks'
+import comics from '../db/DcComics';
+import AppMainLinks from './AppMainLinks';
 
 export default function AppMain() {
     return (
         <>
-            <main className='bg-black'>
-                <div className="align-middle container text-start text-white fs-1 py-5">
-                    -- Content here --
+            <main>
+                <div id='jumbotron'></div>
+                <button id='buttonSeries'>
+                    current series
+                </button>
+                <div className='bg-black row row-cols-xl-6 row-cols-md-4 row-cols-sm-2 row-cols-xs-1 py-4 px-4'>
+                    {comics.map(({ id, title, series, thumb }) => {
+                        return (
+                            <div key={id} className='pb-4'>
+                                <div className='card-img-top'>
+                                    <img className='' src={thumb} alt={title} />
+                                </div>
+                                <div className='card-body'>
+                                    <h6 className='text-white py-2'>{series}</h6>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <button id='buttonLoadMore' className='text-center mx-auto'>
+                        load more
+                    </button>
                 </div>
                 <AppMainLinks />
             </main>
--- src/components/AppMainLinks.jsx
+++ src/components/AppMainLinks.jsx
@@ -1,23 +1,60 @@
+const links = [
+    {
+        id: 1,
+        src: "/img/buy-comics-digital-comics.png",
+        css: "w-70 px-2",
+        alt: "Digital comics",
+        href: "#",
+        name: "DIGITAL COMICS"
+    },
+    {
+        id: 2,
+        src: "/img/buy-comics-merchandise.png",
+        css: "w-70 px-2",
+        alt: "merchandise",
+        href: "#",
+        name: "DC MERCHANDISE"
+    },
+    {
+        id: 3,
+        src: "/img/buy-comics-subscriptions.png",
+        css: "w-70 px-2",
+        alt: "subsciption",
+        href: "#",
+        name: "SUBSCRIPTION"
+    },
+    {
+        id: 4,
+        src: "/img/buy-comics-shop-locator.png",
+        css: "w-50px px-2",
+        alt: "shop locator",
+        href: "#",
+        name: " COMIC SHOP LOCATOR"
+    },
+    {
+        id: 5,
+        src: "/img/buy-dc-power-visa.svg",
+        css: "w-70 px-2",
+        alt: "power visa",
+        href: "#",
+        name: "DC POWER VISA"
+    }
+];
+
 export default function AppMainLinks() {
     return (
         <>
             <div className="bg-primary">
                 <div className="container d-flex justify-content-between align-items-center py-4">
-                    <div>
-                        <img className="w-70 px-2" src="/img/buy-comics-digital-comics.png" alt="Digital comics" /><a className="link-underline link-underline-opacity-0 text-white fs-6 text" href="#">DIGITAL COMICS</a>
-                    </div>
-                    <div>
-                        <img className="w-70 px-2" src="/img/buy-comics-merchandise.png" alt="merchandise" /><a className="link-underline link-underline-opacity-0 text-white fs-6 text" href="#">DC MERCHANDISE</a>
-                    </div>
-                    <div>
-                        <img className="w-70 px-2" src="/img/buy-comics-subscriptions.png" alt="subsciption" /><a className="link-underline link-underline-opacity-0 text-white fs-6 text" href="#">SUBSCRIPTION</a>
-                    </div>
-                    <div>
-                        <img className="w-50px px-2" src="/img/buy-comics-shop-locator.png" alt="shop locator" /><a className="link-underline link-underline-opacity-0 text-white fs-6 text" href="#">COMIC SHOP LOCATOR</a>
-                    </div>
-                    <div>
-                        <img className="w-70 px-2" src="/img/buy-dc-power-visa.svg" alt="power visa" /><a className="link-underline link-underline-opacity-0 text-white fs-6 text" href="#">DC POWER VISA</a>
-                    </div>
+                    {links.map(({ id, css, src, alt, href, name }) => {
+                        return (
+                            <div key={id}>
+                                <img className={css} src={src} alt={alt} />
+                                <a className="link-underline link-underline-opacity-0 text-white fs-6 text" href={href}>
+                                    {name}
+                                </a>
+                            </div>)
+                    })}
                 </div>
             </div >
         </>
