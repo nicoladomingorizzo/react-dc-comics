07f866863b034a877df76642997d2d04372641ff
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-15 17:15:35 +0200

    fixed map inside AppFooter

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -11,41 +11,43 @@ export default function AppFooter() {
                 <div className="container d-flex justify-content-start gap-5">
                     <div className="py-4">
                         <h4 className="py-1 text-white">DC COMICS</h4>
-                        {dcComics.map(({ id, href, name }) => {
-                            return (
-                                <ul key={id} className="list-unstyled">
-                                    <li><a href={href}>{name}</a></li>
-                                </ul>
-                            )
-                        })}
+
+                        <ul className="list-unstyled">
+                            {dcComics.map(({ id, href, name }) => {
+                                return (
+                                    <li key={id} ><a href={href}>{name}</a></li>
+                                )
+                            })}
+                        </ul>
                         <h4 className="py-1 text-white">SHOP</h4>
-                        {shop.map(({ id, href, name }) => {
-                            return (
-                                <ul key={id} className="list-unstyled">
-                                    <li><a href={href}>{name}</a></li>
-                                </ul>
-                            )
-                        })}
+                        <ul className="list-unstyled">
+                            {shop.map(({ id, href, name }) => {
+                                return (
+
+                                    <li key={id}><a href={href}>{name}</a></li>
+                                )
+                            })}
+                        </ul>
                     </div>
                     <div className="py-4">
                         <h4 className="py-1 text-white">DC</h4>
-                        {dc.map(({ id, href, name }) => {
-                            return (
-                                <ul key={id} className="list-unstyled">
-                                    <li><a href={href}>{name}</a></li>
-                                </ul>
-                            )
-                        })}
+                        <ul className="list-unstyled">
+                            {dc.map(({ id, href, name }) => {
+                                return (
+                                    <li key={id}><a href={href}>{name}</a></li>
+                                )
+                            })}
+                        </ul>
                     </div>
                     <div className="py-4">
                         <h4 className="py-1 text-white">SITES</h4>
-                        {sites.map(({ id, href, name }) => {
-                            return (
-                                <ul key={id} className="list-unstyled">
-                                    <li><a href={href}>{name}</a></li>
-                                </ul>
-                            )
-                        })}
+                        <ul className="list-unstyled">
+                            {sites.map(({ id, href, name }) => {
+                                return (
+                                    <li key={id}><a href={href}>{name}</a></li>
+                                )
+                            })}
+                        </ul>
                     </div>
                     <div>
                         <img id="logoFooter" src="/img/dc-logo-bg.png" alt="" />
